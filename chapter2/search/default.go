83680022be06479e7c7e70e8ba52cabc956b5e24
Roy Wei <roywjy@gmail.com>
2017-07-24 01:43:33 +0800

    Example of interface method call restrictions

--- chapter2/search/default.go
+++ chapter2/search/default.go
@@ -31,3 +31,20 @@ func (m defaultMatcher) Search(feed *Feed, searchTerm string) ([]*Result, error)
 // var dm defaultMatch
 // The compiler will reference the dm value to make the call
 // dm.Search(feed, "test")
+
+// Method declared with a pointer receiver of type defaultMatcher
+// func (m *defaultMatcher) Search(feed *Feed, searchTerm string)
+// Call the method via an interface type value
+// var dm defaultMatcher
+// var matcher Matcher = dm     // Assign value to interface type
+// matcher.Search(feed, "test") // Call interface method with value
+// > go build
+// cannot use dm (type defaultMatcher) as type Matcher in assignment
+// Method declared with a value receiver of type defaultMatcher
+// func (m defaultMatcher) Search(feed *Feed, searchTerm string)
+// Call the method via an interface type value
+// var dm defaultMatcher
+// var matcher Matcher = &dm    // Assign pointer to interface type
+// matcher.Search(feed, "test") // Call interface method with pointer
+// > go build
+// Build Successful
