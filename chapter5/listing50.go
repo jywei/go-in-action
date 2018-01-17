a8bab73865ee5d0a3a1288a26f95c9e63e5777ca
Roy Wei <roywjy@gmail.com>
2018-01-17 18:14:07 +0800

    Add notifier interface into listing50

--- chapter5/listing50.go
+++ chapter5/listing50.go
@@ -4,6 +4,12 @@ package main
 
 import "fmt"
 
+// notifier is an interface that defined notification
+// type behavior
+type notifier interface {
+	notify()
+}
+
 // user defines an user in the program
 type user struct {
 	name  string
@@ -35,9 +41,20 @@ func main() {
 		level: "super",
 	}
 
-	// We can access the inner type's method directly
-	ad.user.notify()
+	// // We can access the inner type's method directly
+	// ad.user.notify()
 
-	// The inner type's method is promoted
-	ad.notify()
+	// // The inner type's method is promoted
+	// ad.notify()
+
+	// Send the admin user a notification
+	// The embedded inner type's implementation of the
+	// interface is "promoted" to the outer type
+	sendNotification(&ad)
+}
+
+// sendNotification accepts values that implement the notifier
+// interface and sends notifications
+func sendNotification(n notifier) {
+	n.notify()
 }
